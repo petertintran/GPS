39a57ddf0d41a345eb920bbb166475925fbb1bc8
petertintran <petertintran@gmail.com>
2012-12-04 03:29:37 -0800

    GPS added and working

---
+++ SunriseSunset/CoreLocationController.h
@@ -0,0 +1,13 @@
+//
+//  CoreLocationController.h
+//  SunriseSunset
+//
+//  Created by Lion User on 04/12/2012.
+//  Copyright (c) 2012 Tran.Peter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreLocationController : NSObject
+
+@end
---
+++ SunriseSunset/CoreLocationController.m
@@ -0,0 +1,13 @@
+//
+//  CoreLocationController.m
+//  SunriseSunset
+//
+//  Created by Lion User on 04/12/2012.
+//  Copyright (c) 2012 Tran.Peter. All rights reserved.
+//
+
+#import "CoreLocationController.h"
+
+@implementation CoreLocationController
+
+@end
